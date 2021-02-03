6a8931a6b875572e543b06991ea7200e993047f0
Mu$hr00M <71194731+Mushroom0047@users.noreply.github.com>
2021-02-03 13:00:14 -0300

    formulario prueba 1

--- contacto.php
+++ contacto.php
@@ -4,19 +4,14 @@
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
-    $header = 'From: ' . $mail . " \r\n";
-    $header .= "X-Mailer:PHP/" . phpversion() . " \r\n";
-    $header .= "Mime-Version 1.0 \r\n";
-    $header .= "Content-Type: text/plain";
-    
+    $para = 'contacto@hectorvaldesm.com';
+
     $message = "Este mensaje fue envidao por: " . $name . " \r\n";
     $message .= "Su e-mail es: " .$mail . "\r\n";
-    $message .= "Teléfono de contacto: " . $phone . "\r\n";
     $message .= "Enviado el: " .date('d/m/y', time());
-
-    $para = 'hectorvaldesm47@gmail.com';
 
     mail($para, $subject, utf8_decode($message), $header);
 
+    
     header("Location:index.html");
 ?>
